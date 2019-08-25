e78efb19931ea57a53aa4d73b600dd7982eeaf9e
Steven8519 <851906ejsmal>
2019-08-25 18:43:05 -0500

    initial commit.

---
+++ springreacttraining/src/main/java/com/practice/springbootandreactcourse/SpringbootandreactcourseApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springbootandreactcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootandreactcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootandreactcourseApplication.class, args);
+    }
+
+}
